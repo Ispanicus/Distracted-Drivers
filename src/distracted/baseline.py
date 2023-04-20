a3169564204e79036b5c3305b841da098704f506
Ispanicus <36916264+Ispanicus@users.noreply.github.com>
2023-04-20 14:21:05 +0200

    Update baseline.py

--- src/distracted/baseline.py
+++ src/distracted/baseline.py
@@ -10,41 +10,22 @@ import torch.optim as optim
 from torchvision import transforms
 from torch.optim.lr_scheduler import StepLR
 from distracted.data_util import DATA_PATH, get_train_df, H, W, C, Tensor
-from distracted.dataset_loader import SegmentDataset
+from distracted.dataset_loader import DriverDataset
 import mlflow
 from mlflow import log_metric, log_metrics, log_params, log_artifacts, set_tracking_uri, set_experiment
-
-torch.manual_seed(1)
 
 B = BATCH_SIZE = 64  # 128 For 12GB VRAM
 
 torch.backends.cudnn.benchmark = True
 torch.backends.cuda.matmul.allow_tf32 = True
 
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(2560, 10)
-
-    def forward(self, x, embeddings):
-        print = lambda x: x  # Don't print
-
-        x = self.fc1(embeddings)
-        output = F.log_softmax(x, dim=1)
-        print(f"log_softmax: {torch.mean(output)=}")
-        return output
-
-
-def train(model, device, train_loader, optimizer, epoch, *, log_interval=10, embeddings):
+def train(model, device, train_loader, optimizer, epoch, *, log_interval=10):
     model.train()
-    for batch_idx, (data, target, path_name) in enumerate(train_loader):
-        path_names = [x[:-4] for x in path_name]
-        embs = torch.tensor(embeddings.loc[path_names].values, requires_grad=True)
-        data, target, embs = data.to(device), target.to(device), embs.to(device)
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-        output = model(data, embs)
-        loss = F.nll_loss(output, target)
+        output = model(data)
+        loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
         log_metric("loss", loss.item())
@@ -60,17 +41,15 @@ def train(model, device, train_loader, optimizer, epoch, *, log_interval=10, emb
             )
 
 
-def test(model, device, test_loader, embeddings):
+def test(model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target, path_name in test_loader:
-            path_names = [x[:-4] for x in path_name]
-            embs = torch.tensor(embeddings.loc[path_names].values, requires_grad=True)
-            data, target, embs = data.to(device), target.to(device), embs.to(device)
-            output = model(data, embs)
-            test_loss += F.nll_loss(
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(
                 output, target, reduction="sum"
             ).item()  # sum up batch loss
             pred = output.argmax(
@@ -94,16 +73,11 @@ def permute(x: Tensor[H, W, C]) -> Tensor:
     x: Tensor[C, H, W] = x.permute(2, 0, 1)
     return x
 
-@functools.cache
-def load_embeddings():
-    embeddings = pd.read_parquet(DATA_PATH / "efficientnet_embeddings.parquet")
-    return embeddings
-
 def main():
     torch.manual_seed(42)
 
-    LR = 1
-    GAMMA = 0.7
+    LR = 0.01
+    GAMMA = 1
     EPOCHS = 1
     device = torch.device("cuda")
 
@@ -117,15 +91,28 @@ def main():
 
     transform = transforms.Compose([permute, transforms.Normalize((0,), (1,))])
 
-    segment_train_loader = DataLoader(
-        SegmentDataset("train", transform=transform), **data_kwargs
+    train_loader = DataLoader(
+        DriverDataset("train", returns=["torch_image","label"], transform=transform), **data_kwargs
     )
-    segment_dev_loader = DataLoader(
-        SegmentDataset("dev", transform=transform), **data_kwargs
+    dev_loader = DataLoader(
+        DriverDataset("dev", returns=["torch_image","label"], transform=transform), **data_kwargs
     )
-    embeddings = load_embeddings()
+ 
+    model = EfficientNet.from_pretrained('efficientnet-b0')
 
-    model = Net().to(device)
+    # Set requires_grad to False for all layers except the last two blocks
+    for param in model.parameters():
+        param.requires_grad = False
+
+    num_classes = 10
+    model._fc = nn.Linear(model._fc.in_features, num_classes)
+    for param in model._fc.parameters():
+        param.requires_grad = True
+    for name, param in model.named_parameters():
+        if 'top' in name:
+            param.requires_grad = True
+    model.to(device)
+
     optimizer = optim.Adadelta(model.parameters(), lr=LR, )#rho=0, eps=0, weight_decay=0)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=GAMMA)
@@ -134,18 +121,19 @@ def main():
 
     with mlflow.start_run():
 
+        artifact_path = "model"
         state_dict = model.state_dict()
-        mlflow.pytorch.log_state_dict(state_dict, artifact_path="model")
+
         log_params({"lr": LR, "gamma": GAMMA, "epochs": EPOCHS})
 
         for epoch in range(1, EPOCHS + 1):
-            train(model, device, segment_train_loader, optimizer, epoch, log_interval=10, embeddings=embeddings)
-            test(model, device, segment_dev_loader, embeddings=embeddings)
+            train(model, device, train_loader, optimizer, epoch, log_interval=10)
+            test(model, device, dev_loader)
             
             scheduler.step()
-
-        mlflow.pytorch.log_model(model, "model")
-
+        mlflow.pytorch.log_state_dict(state_dict, artifact_path)
+        mlflow.pytorch.log_model(model, artifact_path)
+    
 
 if __name__ == "__main__":
     main()
