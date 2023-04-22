56dbfdac018b814f4b34e27aa9d7b21a499099de
Ispanicus <36916264+Ispanicus@users.noreply.github.com>
2023-04-22 10:59:17 +0200

    added better metrics, checkpointing

--- src/distracted/baseline.py
+++ src/distracted/baseline.py
@@ -14,13 +14,13 @@ from distracted.dataset_loader import DriverDataset
 from distracted.adapters import get_adapter_model
 from transformers import EfficientNetForImageClassification, EfficientNetImageProcessor
 import mlflow
-from mlflow import log_metric, log_metrics, log_params, log_artifacts, set_tracking_uri, set_experiment
+from mlflow import log_metric, log_params
 
 
 B = BATCH_SIZE = 32 # 128 For 12GB VRAM
 
-MODEL_NAME = "google/efficientnet-b0"
-# MODEL_NAME = "google/efficientnet-b3"
+#MODEL_NAME = "google/efficientnet-b0"
+MODEL_NAME = "google/efficientnet-b3"
 MODEL_NAME_SUFFIX = MODEL_NAME[-2:]
 USE_ADAPTER = True
 MODEL_NAME_MLFOW = MODEL_NAME + "_adapter" if USE_ADAPTER else MODEL_NAME
@@ -28,6 +28,9 @@ ADAPTERS=[(3,7)]
 LR = 2
 GAMMA = 1
 EPOCHS = 10
+best_test_loss = 9999
+sd = ""
+m = ""
 torch.manual_seed(42)
 
 
@@ -47,7 +50,7 @@ def train(model, device, train_loader, optimizer, epoch, *, log_interval=10):
         loss = F.cross_entropy(output.logits, target)
         loss.backward()
         optimizer.step()
-        log_metric("train loss", loss.item())
+        log_metric("train loss", loss.item(), batch_idx+len(train_loader)*epoch)
         if batch_idx % log_interval == 0:
             print(
                 "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
@@ -58,18 +61,19 @@ def train(model, device, train_loader, optimizer, epoch, *, log_interval=10):
                     loss.item(),
                 )
             )
+    test_loss /= len(test_loader.dataset)
 
 
-def test(model, device, test_loader):
+def test(model, device, test_loader, epoch):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for batch_idx, (data, target) in enumerate(test_loader):
             data = preprocessor.preprocess(data, return_tensors="pt")
             data, target = data.to(device), target.to(device)
             output = model(**data)
-            log_metric("val loss", F.cross_entropy(output.logits, target).item())
+            log_metric("val loss", F.cross_entropy(output.logits, target).item(), batch_idx+len(test_loader)*epoch)
             test_loss += F.cross_entropy(
                 output.logits, target, reduction="sum"
             ).item()  # sum up batch loss
@@ -90,15 +94,16 @@ def test(model, device, test_loader):
     )
     log_metric("val accuracy", correct / len(test_loader.dataset))
 
-
-def permute(x: Tensor[H, W, C]) -> Tensor:
-    x: Tensor[C, H, W] = x.permute(2, 0, 1)
-    return x
-
+    if test_loss < best_test_loss:
+        best_test_loss = test_loss
+        sd = state_dict
+        m = model
+    if epoch == EPOCHS:
+        mlflow.pytorch.log_state_dict(sd, artifact_path)
+        mlflow.pytorch.log_model(m, artifact_path)
 
 def main():
 
-    
     device = torch.device("cuda")
 
     data_kwargs = {
@@ -158,12 +163,9 @@ def main():
 
         for epoch in range(1, EPOCHS + 1):
             train(model, device, train_loader, optimizer, epoch, log_interval=10)
-            test(model, device, dev_loader)
+            test(model, device, dev_loader, epoch)
             
             scheduler.step()
-        mlflow.pytorch.log_state_dict(state_dict, artifact_path)
-        mlflow.pytorch.log_model(model, artifact_path)
-    
 
 if __name__ == "__main__":
     main()
