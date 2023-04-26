06aa7c12c40cf3b279c8e3bc75bb4296e1d44423
Johan Laursen <johan7laursen@gmail.com>
2023-04-26 17:47:29 +0200

    id2label added

---
+++ src/distracted/id2label.py
@@ -0,0 +1,23 @@
+ID2LABEL={0: 'normal driving',
+          1: 'texting - right',
+          2: 'talking on the phone - right',
+          3: 'texting - left',
+          4: 'talking on the phone - left',
+          5: 'operating the radio',
+          6: 'drinking',
+          7: 'reaching behind',
+          8: 'hair and makeup',
+          9: 'talking to passenger'
+          }
+
+LABEL2ID={'normal driving': 0,
+            'texting - right': 1,
+            'talking on the phone - right': 2,
+            'texting - left': 3,
+            'talking on the phone - left': 4,
+            'operating the radio': 5,
+            'drinking': 6,
+            'reaching behind': 7,
+            'hair and makeup': 8,
+            'talking to passenger': 9
+            }
